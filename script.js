ad7efbbff38d28634358c633406832a1de2e9b0e
tarundanti-dey <tarundanti@gmail.com>
2026-07-26 18:49:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+// Smooth appearance when scrolling
+
+const sections = document.querySelectorAll("section");
+
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.style.opacity = "1";
+            entry.target.style.transform = "translateY(0)";
+
+        }
+
+    });
+
+}, {
+    threshold:0.2
+});
+
+
+
+sections.forEach(section=>{
+
+    section.style.opacity="0";
+    section.style.transform="translateY(40px)";
+    section.style.transition="1s ease";
+
+    observer.observe(section);
+
+});
